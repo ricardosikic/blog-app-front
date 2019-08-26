4b9ba3e507b0b5b85d280508b83d7b42b680e5f2
ricardo <ricardo.sikic@gmail.com>
2019-08-26 17:57:42 -0400

    modal close/open

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
 let icon = document.getElementById('nav-icon');
 let nav = document.getElementById('nav');
+let contact = document.getElementById('contact-button');
+let modal = document.getElementById('content-modal');
+let sendButton = document.getElementById('send-button');
+
 
 icon.addEventListener('click', () => {
   icon.classList.toggle("change");
   nav.classList.toggle("icon-class");
 });
+
+// CLOSE DE MODAL
+contact.addEventListener('click', () => {
+  console.log(modal);
+  modal.style.display = 'block';
+})
+
+// CLOSE DE MODAL
+sendButton.addEventListener('click', () => {
+  setTimeout(() => {
+    modal.style.display = 'none';
+    modal.style.transition = '2s ease-in';
+  }, 1000);
+})
 
